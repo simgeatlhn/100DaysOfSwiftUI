d3abe97fb3ca5317d05ef82b619f032e96fa3994
simgeatlhn <simgeeatlihan@gmail.com>
2022-02-03 19:36:37 +0300

    Unit Conversion (first challange)

---
+++ UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by simge on 3.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
