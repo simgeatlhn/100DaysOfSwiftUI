6c65b1047aa07b38440c68d0919180bbbde85259
simgeatlhn <simgeeatlihan@gmail.com>
2022-02-10 14:25:08 +0300

    Better Rest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by simge on 9.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
