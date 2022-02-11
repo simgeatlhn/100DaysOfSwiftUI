3121b598f37264d6a207bf5e3988291370aa77ec
simgeatlhn <simgeeatlihan@gmail.com>
2022-02-11 15:13:54 +0300

    Animation

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by simge on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
