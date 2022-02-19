21696a2d9be9235040b9e2921302751ca3cceafd
simgeatlhn <simgeeatlihan@gmail.com>
2022-02-19 17:34:33 +0300

    Drawing

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by simge on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
