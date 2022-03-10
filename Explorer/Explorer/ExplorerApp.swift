93ccbb51c97dd60a790af43b92b719465120d465
simgeatlhn <simgeeatlihan@gmail.com>
2022-03-10 13:48:28 +0300

    Explorer

---
+++ Explorer/Explorer/ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorerApp.swift
+//  Explorer
+//
+//  Created by simge on 10.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
