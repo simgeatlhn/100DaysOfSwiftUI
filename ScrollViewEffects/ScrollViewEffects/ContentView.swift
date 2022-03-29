ec853329b830ea796c79213f9fbabb9e9dcbeed7
simgeatlhn <simgeeatlihan@gmail.com>
2022-03-29 21:24:07 +0300

    ScrollViewEffects

---
+++ ScrollViewEffects/ScrollViewEffects/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  ScrollViewEffects
+//
+//  Created by simge on 29.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("simge \(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollViewEffects/ScrollViewEffects/ScrollViewEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewEffectsApp.swift
+//  ScrollViewEffects
+//
+//  Created by simge on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
